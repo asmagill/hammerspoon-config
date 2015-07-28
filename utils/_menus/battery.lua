e0be78983cfd43c0d00df87f909348be8ed6d27e
asmagill <asmagill@users.noreply.github.com>
2015-07-28 10:26:10 -0500

    testing battery warnings

--- utils/_menus/battery.lua
+++ utils/_menus/battery.lua
@@ -12,8 +12,9 @@ local module = {
     ]],
     _TODO        = [[
 
-          Possible additions -- issue warning for low battery?  System already does this, so...
-                                change menu title to icon with color change for battery state?
+       [x] issue warning for low battery?  System doesn't if we hide it's icon...
+       [ ] change menu title to icon with color change for battery state?
+              awaiting way to composite drawing objects...
 
     ]],
     _LICENSE     = [[ See README.md ]]
@@ -31,7 +32,56 @@ local onBattery = utf8.codepointToUTF8(0x1F50B) -- battery
 local menuUserData = nil
 local currentPowerSource = ""
 
-local powerSourceChangeFN = function()
+-- Some "notifications" to apply... need to update battery watcher to do these
+module.batteryNotifications = {
+    { onBattery = true, percentage = 10, doEvery = false,
+        fn = function()
+            local audio = require("hs.audiodevice").defaultOutputDevice()
+            local volume, muted = audio:volume(), audio:muted()
+            -- apparently some devices don't have a volume or mute...
+            if volume then audio:setVolume(100) end
+            if muted then audio:setMuted(false) end
+            os.execute([[ say -v "Zarvox" "LOW BATTERY" ]])
+            if volume then audio:setVolume(volume) end
+            if muted then audio:setMuted(true) end
+        end
+    },
+    { onBattery = true, percentage = 5, doEvery = 60,
+        fn = function()
+            local audio = require("hs.audiodevice").defaultOutputDevice()
+            local volume, muted = audio:volume(), audio:muted()
+            -- apparently some devices don't have a volume or mute...
+            if volume then audio:setVolume(100) end
+            if muted then audio:setMuted(false) end
+            os.execute([[ say -v "Zarvox" "PLUG ME IN NOW" ]])
+            if volume then audio:setVolume(volume) end
+            if muted then audio:setMuted(true) end
+        end
+    },
+    { onBattery = true, timeRemaining = 30, doEvery = 300,
+        fn = function()
+            local alert = require("hs.alert")
+            local battery = require("hs.battery")
+            alert.show("Battery has "..tostring(math.floor(battery.timeRemaining())).." minutes left...", 10)
+        end
+    },
+    { onBattery = false, percentage = 10, doEvery = false,
+        fn = function()
+        -- I don't care if I miss this one, so... no volume changes
+            os.execute([[ say -v "Zarvox" "Feeling returning to my circuits" ]])
+        end
+    },
+    { onBattery = false, percentage = 90, doEvery = false,
+        fn = function()
+        -- I don't care if I miss this one, so... no volume changes
+            os.execute([==[ say -v "Zarvox" "I'm feeling [[inpt PHON; rate 80]]+mUXC[[inpt TEXT; rset 0]] better [[emph +]]now" ]==])
+        end
+    },
+}
+
+local notificationStatus = {}
+
+local powerSourceChangeFN = function(justOn)
     local newPowerSource = battery.powerSource()
 
     if currentPowerSource ~= newPowerSource then
@@ -44,6 +94,48 @@ local powerSourceChangeFN = function()
             end
         end
     end
+    if not justOn then
+        local test = {
+            percentage = battery.percentage(),
+            onBattery = battery.powerSource() == "Battery Power",
+            timeRemaining = battery.timeRemaining(),
+            timeStamp = os.time()
+        }
+
+        for i,v in ipairs(module.batteryNotifications) do
+            if v.onBattery == test.onBattery then
+                local shouldWeDoSomething = false
+                if not notificationStatus[i] then
+                    if v.percentage then
+                        if v.onBattery then
+                            shouldWeDoSomething = (test.percentage - v.percentage) < 0
+                        else
+                            shouldWeDoSomething = (test.percentage - v.percentage) > 0
+                        end
+                    elseif v.timeRemaining then
+                        if v.onBattery then
+                            shouldWeDoSomething = (test.timeRemaining - v.timeRemaining) < 0
+                        else
+                            shouldWeDoSomething = (test.timeRemaining - v.timeRemaining) > 0
+                        end
+                    else
+                        print("++ unknown test for battery notification #"..tostring(i))
+                    end
+                elseif notificationStatus[i] and doEvery and
+                  (test.timeStamp - notificationStatus[i]) > v.doEvery then
+                      shouldWeDoSomething = true
+                end
+--                print("++ "..tostring(i).." -- "..hs.inspect(v))
+                if shouldWeDoSomething then
+                    notificationStatus[i] = test.timeStamp
+                    v.fn()
+                end
+            else
+            -- remove stored status for wrong onBattery types...
+                if notificationStatus[i] then notificationStatus[i] = nil end
+            end
+        end
+    end
 end
 
 local powerWatcher = battery.watcher.new(powerSourceChangeFN)
@@ -52,10 +144,6 @@ local displayBatteryData = function(modifier)
     local menuTable = {}
 
     local pwrIcon = (battery.powerSource() == "AC Power") and onAC or onBattery
---    table.insert(menuTable, { title = pwrIcon.."  "..battery.powerSource() })
---
---    table.insert(menuTable, {
---        title = utf8.codepointToUTF8(0x1F6A6).."  "..(
     table.insert(menuTable, { title = pwrIcon.."  "..(
             (battery.isCharged()  and "Fully Charged") or
             (battery.isCharging() and (battery.isFinishingCharge() and "Finishing Charge" or "Charging")) or
@@ -114,7 +202,7 @@ module.menuUserdata = menuUserdata -- for debugging, may remove in the future
 module.start = function()
     menuUserData, currentPowerSource = menubar.new(), ""
 
-    powerSourceChangeFN()
+    powerSourceChangeFN(true)
     powerWatcher:start()
 
     menuUserData:setMenu(displayBatteryData)
