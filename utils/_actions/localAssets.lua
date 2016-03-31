fa7e91142c2c237174b3f8680292e5bd5eeba3eb
asmagill <asmagill@users.noreply.github.com>
2016-03-30 22:56:40 -0500

    simplify web server

--- utils/_actions/localAssets.lua
+++ utils/_actions/localAssets.lua
@@ -8,13 +8,18 @@ module.documentRoot = hs.configdir.."/_localAssets"
 
 local httpserver = require("hs.httpserver")
 local fs         = require("hs.fs")
+local utf8       = require("hs.utf8")
 
 local serverPort = 7734
 
 -- private variables and methods -----------------------------------------
 
-module.server = httpserver.new():setPort(serverPort):setCallback(function(method, path)
-    local file = module.documentRoot..path
+module.server = httpserver.new():setPort(serverPort):setCallback(function(method, path, headers, body)
+    if headers.Host ~= "localhost:"..tostring(serverPort) then
+    return "<html><head><title>Forbidden</title><head><body><H1>HTTP/1.1 403 Forbidden</H1></body></html>", 403, {}
+    end
+
+    local file = module.documentRoot .. path
     local attr = fs.attributes(file)
     if attr and attr.mode == "file" then
         local finput = io.open(file, "rb")
@@ -25,29 +30,31 @@ module.server = httpserver.new():setPort(serverPort):setCallback(function(method
         local data = [[
             <html>
               <head>
-                <title>Directory listing for "..path.."</title>
+                <title>Directory listing for ]] .. path .. [[</title>
               </head>
               <body>
-                <h1>Directory listing for ]]..path..[[</h1>
+                <h1>Directory listing for ]] .. path .. [[</h1>
                 <hr>
                 <pre>]]
 
         for k in fs.dir(file) do
             local fattr = fs.attributes(file.."/"..k)
-            if fattr then
-                data = data .. string.format("    %s %s %7.2fK %s\n", fattr.mode:sub(1,1), fattr.permissions, fattr.size / 1024, k)
-            else
-                data = data .. "    ?? "..k.." ??\n"
+            if k:sub(1,1) ~= "." then
+                if fattr then
+                    data = data .. string.format("    %-12s %s %7.2fK <a href=\"http://" .. headers.Host .. path .. "/%s\">%s</a>\n", fattr.mode, fattr.permissions, fattr.size / 1024, k, k)
+                else
+                    data = data .. "    ?? " .. k .. " ??\n"
+                end
             end
         end
         data = data .. [[</pre>
                 <hr>
-                <div align="right"><i>Minimalist Hammerspoon Local Assets Server; ]]..os.date()..[[</i></div>
+                <div align="right"><i>Minimalist Hammerspoon Local Assets Server; ]] .. os.date() .. [[</i></div>
               </body>
             </html>]]
         return data, 200, {}
     else
-        return "<html><head><title>Object Not Found</title><head><body><H1>HTTP/1.1 404 Object Not Found</H1><br/>The requested URL was not found on this server.<br/><hr/></body></html>", 404, {}
+        return "<html><head><title>Object Not Found</title><head><body><H1>HTTP/1.1 404 Object Not Found</H1><br/>The requested URL, http://" .. headers.Host .. path .. ", was not found on this server.<br/><hr/></body></html>", 404, {}
     end
 end):start()
 
