1c1cb21fb76239e380144a964ad74c95ce89b8b9
asmagill <asmagill@users.noreply.github.com>
2016-04-03 03:41:22 -0500

    still experimenting

--- utils/_actions/localAssets.lua
+++ utils/_actions/localAssets.lua
@@ -9,15 +9,16 @@ module.documentRoot = hs.configdir.."/_localAssets"
 local httpserver = require("hs.httpserver")
 local fs         = require("hs.fs")
 local utf8       = require("hs.utf8")
+local inspect    = require("hs.inspect")
 
 local serverPort = 7734
 
 -- private variables and methods -----------------------------------------
 
-module.server = httpserver.new():setPort(serverPort):setCallback(function(method, path, headers, body)
-    if headers.Host ~= "localhost:"..tostring(serverPort) then
-    return "<html><head><title>Forbidden</title><head><body><H1>HTTP/1.1 403 Forbidden</H1></body></html>", 403, {}
-    end
+module.server = httpserver.new(false, false):setPort(serverPort):setCallback(function(method, path, headers, body)
+--     if headers.Host ~= "localhost:"..tostring(serverPort) then
+--     return "<html><head><title>Forbidden</title><head><body><H1>HTTP/1.1 403 Forbidden</H1></body></html>", 403, {}
+--     end
 
     local file = module.documentRoot .. path
     local attr = fs.attributes(file)
@@ -41,13 +42,16 @@ module.server = httpserver.new():setPort(serverPort):setCallback(function(method
             local fattr = fs.attributes(file.."/"..k)
             if k:sub(1,1) ~= "." then
                 if fattr then
-                    data = data .. string.format("    %-12s %s %7.2fK <a href=\"http://" .. headers.Host .. path .. "/%s\">%s</a>\n", fattr.mode, fattr.permissions, fattr.size / 1024, k, k)
+                    data = data .. string.format("    %-12s %s %7.2fK <a href=\"http://%s%s%s\">%s</a>\n", fattr.mode, fattr.permissions, fattr.size / 1024, headers.Host, path, k, k)
                 else
                     data = data .. "    ?? " .. k .. " ??\n"
                 end
             end
         end
-        data = data .. [[</pre>
+        data = data .. "</pre>"
+        data = data .. "<hr>Message Body<br><pre>" .. utf8.hexDump(body) .. "</pre>"
+        data = data .. "<hr>Message Header<pre>" .. inspect(headers) .. "</pre>"
+        data = data .. [[
                 <hr>
                 <div align="right"><i>Minimalist Hammerspoon Local Assets Server; ]] .. os.date() .. [[</i></div>
               </body>
