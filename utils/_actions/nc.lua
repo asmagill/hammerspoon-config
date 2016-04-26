ddf5e37f6dbca7950e656765cc5171db5f1b1939
asmagill <asmagill@users.noreply.github.com>
2016-04-26 00:30:01 -0500

    try out core module

--- utils/_actions/nc.lua
+++ utils/_actions/nc.lua
@@ -23,7 +23,9 @@ local ignoreInWorkspaceObserver = {
     NSWorkspaceActiveDisplayDidChangeNotification = true,
 }
 
-local nc = require("hs._asm.notificationcenter")
+local nc                       = require("hs._asm.notificationcenter")
+local distributednotifications = require"hs.distributednotifications"
+
 module.workspaceObserver = nc.workspaceObserver(function(n,o,i)
     if not ignoreInWorkspaceObserver[n] then
         local f = io.open("__workspaceobserver.txt","a") ;
@@ -32,8 +34,14 @@ module.workspaceObserver = nc.workspaceObserver(function(n,o,i)
     end
 end):start()
 
-module.distributedObserver = nc.distributedObserver(function(n,o,i)
-    local f = io.open("__distributedobserver.txt","a") ;
+-- module.distributedObserver = nc.distributedObserver(function(n,o,i)
+--     local f = io.open("__distributedobserver.txt","a") ;
+--     f:write(os.date().."\t".."name:"..n.."\tobj:"..inspect(o):gsub("%s+"," ").."\tinfo:"..inspect(i):gsub("%s+"," ").."\n")
+--     f:close()
+-- end):start()
+
+module.distributedObserver_core = distributednotifications.new(function(n,o,i)
+    local f = io.open("__distributedobserver_core.txt","a") ;
     f:write(os.date().."\t".."name:"..n.."\tobj:"..inspect(o):gsub("%s+"," ").."\tinfo:"..inspect(i):gsub("%s+"," ").."\n")
     f:close()
 end):start()
