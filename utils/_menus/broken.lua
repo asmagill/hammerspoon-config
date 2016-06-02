5001da83da2e422ea2582dc3f5929234e288f173
asmagill <asmagill@users.noreply.github.com>
2016-06-01 23:55:28 -0500

    a little more complex of an example

--- utils/_menus/broken.lua
+++ utils/_menus/broken.lua
@@ -1,11 +1,18 @@
 local menubar = require"hs.menubar"
 local image   = require"hs.image"
 
-local maxMenus = 3
+local maxMenus = 5
 
-local brokenMenu = menubar.new():setIcon(image.imageFromPath(hs.configdir .. "/_localAssets/psychotic.png"):setSize({w=22,h=22}))
+local brokenMenu = menubar.new():setIcon(
+    image.imageFromPath(hs.configdir .. "/_localAssets/psychotic.png"):setSize({w=22,h=22})
+)
 
 local menus = {}
+
+local switchToMenu = function(menuNumber)
+    brokenMenu:setMenu(menus[menuNumber])
+    brokenMenu:setTitle(menuNumber)
+end
 
 for i = 1, maxMenus, 1 do
     local nextMenu = (i == maxMenus) and 1 or (i + 1)
@@ -14,14 +21,26 @@ for i = 1, maxMenus, 1 do
         { title = "-" },
         {
             title = "Switch to " .. tostring(nextMenu),
-            fn = function(...)
-                brokenMenu:setMenu(menus[nextMenu])
-            end,
+            fn = function(...) switchToMenu(nextMenu) end,
+        },
+        { title = "-" },
+        {
+            title = "Jump to...",
+            menu = {
+                {
+                    title = "first",
+                    fn = function(...) switchToMenu(1) end,
+                },
+                {
+                    title = "last",
+                    fn = function(...) switchToMenu(maxMenus) end,
+                },
+            },
         },
     })
 end
 
-brokenMenu:setMenu(menus[1])
+switchToMenu(1)
 
 return {
     brokenMenu = brokenMenu,
