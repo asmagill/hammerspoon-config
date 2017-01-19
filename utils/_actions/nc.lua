5de2e9acdb969c977f0ae5adbedc3e526fa898d3
asmagill <asmagill@users.noreply.github.com>
2017-01-18 22:12:34 -0600

    ok, I do use it, but keep log reasonable

--- utils/_actions/nc.lua
+++ utils/_actions/nc.lua
@@ -23,8 +23,9 @@ local ignoreInWorkspaceObserver = {
     NSWorkspaceActiveDisplayDidChangeNotification = true,
 }
 
-local nc                       = require("hs._asm.notificationcenter")
-local distributednotifications = require"hs.distributednotifications"
+local nc                       = require "hs._asm.notificationcenter"
+local distributednotifications = require "hs.distributednotifications"
+local fnutils                  = require "hs.fnutils"
 
 module.workspaceObserver = nc.workspaceObserver(function(n,o,i)
     if not ignoreInWorkspaceObserver[n] then
@@ -34,16 +35,32 @@ module.workspaceObserver = nc.workspaceObserver(function(n,o,i)
     end
 end):start()
 
--- module.distributedObserver = nc.distributedObserver(function(n,o,i)
---     local f = io.open("__distributedobserver.txt","a") ;
---     f:write(os.date().."\t".."name:"..n.."\tobj:"..inspect(o):gsub("%s+"," ").."\tinfo:"..inspect(i):gsub("%s+"," ").."\n")
---     f:close()
--- end):start()
+local do_logFile = "__distributedobserver_core.log"
+
+local f, err = io.open(do_logFile, "r")
+if f then
+    local logData = f:read("a")
+    f:close()
+    local asArray = fnutils.split(logData, "[\r\n]")
+    if #asArray > 1000 then
+        local newArray = {}
+        table.move(asArray, #asArray - 1000, #asArray, 1, newArray)
+        f, err = io.open(do_logFile, "w")
+        if f then
+            f:write(table.concat(newArray, "\n"))
+            f:close()
+        else
+            print("unable to create truncated " .. do_logFile .. " (" .. err ..")")
+        end
+    end
+else
+    print("unable to read " .. do_logFile .. " to check length (" .. err ..")")
+end
 
 module.distributedObserver_core = distributednotifications.new(function(n,o,i)
-    local f = io.open("__distributedobserver_core.txt","a") ;
-    f:write(os.date().."\t".."name:"..inspect(n).."\tobj:"..inspect(o):gsub("%s+"," ").."\tinfo:"..inspect(i):gsub("%s+"," ").."\n")
+    local f = io.open(do_logFile,"a") ;
+    f:write(timestamp().."\t".."name:"..inspect(n).."\tobj:"..inspect(o):gsub("%s+"," ").."\tinfo:"..inspect(i):gsub("%s+"," ").."\n")
     f:close()
-end)-- :start() -- gets big fast, so let me turn it on when I want to explore
+end):start() -- gets big fast, so let me turn it on when I want to explore
 
 return module
