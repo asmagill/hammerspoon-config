6f16e9118c4100ce8b8e5eefe2d23c5099a11392
asmagill <asmagill@users.noreply.github.com>
2017-02-02 17:41:42 -0600

    more robust, but still misses two stage launchers like java apps

--- _scratch/focusedWindowChange.lua
+++ _scratch/focusedWindowChange.lua
@@ -8,7 +8,7 @@ module.actionFunction = function(win)
     if win then
         print(string.format("%s -- focused window change: %s (%s)", os.date("%F %T"), win:title(), win:application():name()))
     else
-        print(string.format("%s -- focused window change: %s (%s)", os.date("%F %T"), "** no window **", application.frontmostApplication()))
+        print(string.format("%s -- focused window change: %s (%s)", os.date("%F %T"), "** no window **", application.frontmostApplication():name()))
     end
 end
 
@@ -18,7 +18,14 @@ local newWatcher = function(andNotify)
     local win = window.focusedWindow()
     if andNotify then module.actionFunction(win) end
     local app = win and win:application() or application.frontmostApplication()
-    local watcher = app:newWatcher(watcherFunction):start({uielement.watcher.applicationDeactivated, uielement.watcher.focusedWindowChanged})
+    local watcher = app:newWatcher(watcherFunction):start({uielement.watcher.applicationDeactivated, uielement.watcher.focusedWindowChanged, uielement.watcher.elementDestroyed})
+
+    -- this is annoying... quitting an application doesn't destroy it's uielement, so we can't
+    -- add just add uielement.watcher.elementDestroyed to the hs.uielement watcher... we have to
+    -- store the application's pid so hs.application.watcher can detect the termination and force
+    -- an update for us
+    module.activePID = app:pid()
+
     return watcher
 end
 
@@ -37,6 +44,13 @@ watcherFunction = function(el, ev, ...)
 end
 
 module.watcher = newWatcher()
+
+module.terminationWatcher = application.watcher.new(function(n, e, o)
+    if e == application.watcher.terminated and o:pid() == module.activePID then
+        -- force an applicationDeactivated event so that it rebuilds the watcher.
+        watcherFunction(nil, uielement.watcher.applicationDeactivated)
+    end
+end):start()
 
 return module
 
