b754ea890206673ec5bd2d1bb32301cc1f000040
asmagill <asmagill@users.noreply.github.com>
2017-09-21 23:25:39 -0500

    migrate to new syntax

--- _scratch/textfieldExamples.lua
+++ _scratch/textfieldExamples.lua
@@ -41,20 +41,20 @@ local gui = guitk.new{x = 100, y = 100, h = 300, w = 300 }:show()
 local manager = guitk.manager.new()
 gui:contentManager(manager)
 
-manager:add(guitk.element.textfield.newLabel("I am a label, not selectable"):tooltip("newLabel"))
-manager:add(guitk.element.textfield.newLabel(styledtext.new({
+manager:insert(guitk.element.textfield.newLabel("I am a label, not selectable"):tooltip("newLabel"))
+manager:insert(guitk.element.textfield.newLabel(styledtext.new({
     "I am a StyledText selectable label",
     { starts = 8,  ends = 13, attributes = { color = { red  = 1 }, font = { name = "Helvetica-Bold", size = 12 } } },
     { starts = 14, ends = 17, attributes = { color = { blue = 1 }, font = { name = "Helvetica-Oblique", size = 12 } } },
     { starts = 19, ends = 28, attributes = { strikethroughStyle = styledtext.lineAppliesTo.word | styledtext.lineStyles.single } },
 })):tooltip("newLabel with styledtext"))
-manager:add(guitk.element.textfield.newTextField("I am a text field"):tooltip("newTextField"):editingCallback(editFn))
-manager:add(guitk.element.textfield.newWrappingLabel("I am a wrapping label\nthe only difference so far is that I'm selectable"):tooltip("newWrappingLabel -- still trying to figure out what that means"))
+manager:insert(guitk.element.textfield.newTextField("I am a text field"):tooltip("newTextField"):editingCallback(editFn), { w = 200 })
+manager:insert(guitk.element.textfield.newWrappingLabel("I am a wrapping label\nthe only difference so far is that I'm selectable"):tooltip("newWrappingLabel -- still trying to figure out what that means"))
 
 -- testing tab/shift-tab works; note if you're testing this before I create formal releases, this required a change to
 -- the root module (hs._asm.guitk) as well, so you'll need to recompile that too.
-manager:add(guitk.element.textfield.newTextField("Another one!"):tooltip("added for tabbing"):editingCallback(editFn))
-manager:add(guitk.element.textfield.newTextField("Another two!"):tooltip("and shift-tabbing"):editingCallback(editFn))
+manager:insert(guitk.element.textfield.newTextField("Another one!"):tooltip("added for tabbing"):editingCallback(editFn), { w = 200 })
+manager:insert(guitk.element.textfield.newTextField("Another two!"):tooltip("and shift-tabbing"):editingCallback(editFn), { w = 200 })
 
 module.manager = manager
 
